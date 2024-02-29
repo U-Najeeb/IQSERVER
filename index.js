0107f533ca6b6a7edd43959b0ed312b03576bb73
Sheikh Umair <umair.najeeb@iquasar.com>
2024-02-29 19:44:35 +0530

    FIXEDd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv").config({ path: "./config.env" });
 const mongoose = require("mongoose");
-const { Server } = require("socket.io")
+const { Server } = require("socket.io");
 
 const app = require("./app.js");
 
@@ -18,39 +18,38 @@ const server = app.listen(PORT, () => {
 const liveDrivers = new Map();
 // const liveDrivers = new Set();
 
-// SOCKET SERVER 
+// SOCKET SERVER
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
-  }
-})
+    origin: "*",
+  },
+});
 
 io.on("connection", (socket) => {
-  console.log(socket.id + " joined")
+  console.log(socket.id + " joined");
 
   socket.on("live-drivers", (driverData) => {
     // socket.broadcast.emit("live-drivers", "asdasd")
     // if (!liveDrivers.has(socket.id)) {
-    liveDrivers.set(socket.id, driverData)
+    liveDrivers.set(socket.id, driverData);
     // liveDrivers.add(driverData)
 
     // }
-    console.log("live driver _____>", liveDrivers)
+    console.log("live driver _____>", liveDrivers);
     // console.log(driverData)
-    io.emit("live-drivers", Array.from(liveDrivers))
+    io.emit("live-drivers", Array.from(liveDrivers));
     // io.emit("live-drivers", liveDrivers)
-  })
+  });
 
   socket.on("SOS", (sosDetails) => {
-    console.log(sosDetails)
-    io.emit("SOS", sosDetails)
-  })
+    console.log(sosDetails);
+    io.emit("SOS", sosDetails);
+  });
 
   socket.on("disconnect", () => {
-    liveDrivers.delete(socket.id)
+    liveDrivers.delete(socket.id);
     console.log(socket.id, "A user disconnected");
     console.log(liveDrivers, "New Map");
   });
-}
-)
-console.log(liveDrivers)
+});
+console.log(liveDrivers);
